10f3adf91201ef81fb7f3b80c7015787f6bb6f09
mazacon <mazacon@protonmail.com>
2023-05-10 22:21:27 -0500

    add Chirp BelongsTo User relationship to model

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     protected $fillable = [
         'message',
